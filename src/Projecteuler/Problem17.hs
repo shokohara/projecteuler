72ec2031ec6103939c7755c34aa693841a806f82
shokohara <shokohara@users.noreply.github.com>
2017-02-06 09:06:29 +0900

    .

--- src/Projecteuler/Problem17.hs
+++ src/Projecteuler/Problem17.hs
@@ -4,20 +4,20 @@ resolve :: Integer
 resolve = toInteger $ f 1000
 
 f :: Int -> Int
-f n = length . filter (' ' /=) . filter ('-' /=) . concat $ f2 . f3 <$> [1..n]
+f n = length . filter (' ' /=) . filter ('-' /=) . concat $ f2 <$> [1..n]
 
 f3 :: Int -> (Int, Int, Int, Int)
 f3 n = (n `mod` 10000 `div` 1000, n `mod` 1000 `div` 100, n `mod` 100 `div` 10, n `mod` 10)
 
-f2 :: (Int, Int, Int, Int) -> String
-f2 (a, b, c, d) =
-  let n = a * 1000 + b * 100 + c * 10 + d in
-      fa a b c d ++
-        (if 1000 <= n `mod` 10000 && 0 < n `mod` 1000 then " " else "") ++
-          fb b c d ++
-            (if 100 <= n `mod` 1000 && 0 < n `mod` 100 then " " else "") ++
-              fc c d ++
-                if n `mod` 100 == 0 || n `mod` 100 `div` 10 == 1 || n `mod` 10 == 0 then "" else (if c == 0 then "" else "-") ++ fe d
+f2 :: Int -> String
+f2 n =
+  fa n ++
+    (if 1000 <= n `mod` 10000 && 0 < n `mod` 1000 then " " else "") ++
+      fb (n `mod` 1000 `div` 100) (n `mod` 100 `div` 10) (n `mod` 10) ++
+        (if 100 <= n `mod` 1000 && 0 < n `mod` 100 then " " else "") ++
+          fc (n `mod` 100) ++
+            (if n `mod` 100 `div` 10 == 0 || n `mod` 100 `div` 10 == 1 || (n `mod` 100 `div` 10 > 1 && n `mod` 10 == 0) then "" else "-" ++ fe (n `mod` 10)) ++
+              (if n `mod` 100 `div` 10 == 0 then fe (n `mod` 10) else "")
   where
     fe 1 = "one"
     fe 2 = "two"
@@ -29,30 +29,32 @@ f2 (a, b, c, d) =
     fe 8 = "eight"
     fe 9 = "nine"
     fe _ = ""
-    fc 1 0 = "ten"
-    fc 1 1 = "eleven"
-    fc 1 2 = "twelve"
-    fc 1 3 = "thirteen"
-    fc 1 4 = "fourteen"
-    fc 1 5 = "fifteen"
-    fc 1 6 = "sixteen"
-    fc 1 7 = "seventeen"
-    fc 1 8 = "eighteen"
-    fc 1 9 = "nineteen"
-    fc 2 _ = "twenty"
-    fc 3 _ = "thirty"
-    fc 4 _ = "forty"
-    fc 5 _ = "fifty"
-    fc 6 _ = "sixty"
-    fc 7 _ = "seventy"
-    fc 8 _ = "eithty"
-    fc 9 _ = "ninety"
-    fc _ _ = ""
+    fc x
+      | x < 10 = ""
+      | x == 10 = "ten"
+      | x == 11 = "eleven"
+      | x == 12 = "twelve"
+      | x == 13 = "thirteen"
+      | x == 14 = "fourteen"
+      | x == 15 = "fifteen"
+      | x == 16 = "sixteen"
+      | x == 17 = "seventeen"
+      | x == 18 = "eighteen"
+      | x == 19 = "nineteen"
+      | x < 30 = "twenty"
+      | x < 40 = "thirty"
+      | x < 50 = "forty"
+      | x < 60 = "fifty"
+      | x < 70 = "sixty"
+      | x < 80 = "seventy"
+      | x < 90 = "eithty"
+      | x < 100 = "ninety"
+      | otherwise = ""
     fb 0 _ _ = ""
     fb x 0 0 = fe x ++ " hundred"
     fb x _ _ = fe x ++ " hundred and"
-    fa 0 _ _ _ = ""
-    fa x 0 0 0 = fe x ++ " thousand"
-    fa x _ _ _ = fe x ++ " thousand and"
-
+    fa x
+      | x `mod` 10000 `div` 1000 == 0 = ""
+      | n `mod` 1000 `div` 100 == 0 && n `mod` 100 `div` 10 == 0 && n `mod` 10 == 0 = fe (n `mod` 10000 `div` 1000) ++ " thousand"
+      | otherwise = fe (n `mod` 10000 `div` 1000) ++ " thousand and"
 
--- test/Projecteuler/Problem17Spec.hs
+++ test/Projecteuler/Problem17Spec.hs
@@ -5,36 +5,40 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
+  it "moddiv" $ 256 `mod` 10 == 6
+  it "moddiv" $ 256 `mod` 100 `div` 10 == 5
+  it "moddiv" $ 256 `mod` 1000 `div` 100 == 2
+  it "moddiv" $ 1024 `mod` 10000 `div` 1000 == 1
   it "f" $ f 1 `shouldBe` 3
   it "f" $ f 5 `shouldBe` 19
   it "f" $ f 5 `shouldBe` 19
   it "f" $ f 1000 `shouldBe` 21124
-  it "f2" $ f2 (0, 0, 0, 1) `shouldBe` "one"
-  it "f2" $ f2 (0, 0, 0, 5) `shouldBe` "five"
-  it "f2" $ f2 (0, 0, 1, 0) `shouldBe` "ten"
-  it "f2" $ f2 (0, 0, 1, 1) `shouldBe` "eleven"
-  it "f2" $ f2 (0, 0, 2, 0) `shouldBe` "twenty"
-  it "f2" $ f2 (0, 0, 2, 1) `shouldBe` "twenty-one"
-  it "f2" $ f2 (0, 0, 5, 0) `shouldBe` "fifty"
-  it "f2" $ f2 (0, 1, 0, 0) `shouldBe` "one hundred"
-  it "f2" $ f2 (0, 1, 0, 1) `shouldBe` "one hundred and one"
-  it "f2" $ f2 (0, 1, 0, 2) `shouldBe` "one hundred and two"
-  it "f2" $ f2 (0, 2, 0, 0) `shouldBe` "two hundred"
-  it "f2" $ f2 (0, 1, 1, 1) `shouldBe` "one hundred and eleven"
-  it "f2" $ f2 (0, 1, 1, 5) `shouldBe` "one hundred and fifteen"
-  it "f2" $ f2 (0, 9, 0, 1) `shouldBe` "nine hundred and one"
-  it "f2" $ f2 (0, 3, 4, 2) `shouldBe` "three hundred and forty-two"
-  it "f2" $ f2 (1, 0, 0, 0) `shouldBe` "one thousand"
-  it "f2" $ f2 (1, 0, 0, 1) `shouldBe` "one thousand and one"
-  it "f2" $ f2 (1, 0, 0, 2) `shouldBe` "one thousand and two"
-  it "f2" $ f2 (1, 0, 1, 1) `shouldBe` "one thousand and eleven"
-  it "f2" $ f2 (1, 0, 2, 1) `shouldBe` "one thousand and twenty-one"
-  it "f2" $ f2 (1, 1, 2, 1) `shouldBe` "one thousand and one hundred and twenty-one"
-  it "f2" $ f2 (1, 1, 1, 1) `shouldBe` "one thousand and one hundred and eleven"
-  it "f2" $ f2 (2, 0, 0, 0) `shouldBe` "two thousand"
-  it "f2" $ f2 (2, 0, 0, 1) `shouldBe` "two thousand and one"
-  it "f2" $ f2 (2, 0, 0, 2) `shouldBe` "two thousand and two"
-  it "f2" $ f2 (2, 1, 1, 1) `shouldBe` "two thousand and one hundred and eleven"
+  it "f2" $ f2 0001 `shouldBe` "one"
+  it "f2" $ f2 0005 `shouldBe` "five"
+  it "f2" $ f2 0010 `shouldBe` "ten"
+  it "f2" $ f2 0011 `shouldBe` "eleven"
+  it "f2" $ f2 0020 `shouldBe` "twenty"
+  it "f2" $ f2 0021 `shouldBe` "twenty-one"
+  it "f2" $ f2 0050 `shouldBe` "fifty"
+  it "f2" $ f2 0100 `shouldBe` "one hundred"
+  it "f2" $ f2 0101 `shouldBe` "one hundred and one"
+  it "f2" $ f2 0102 `shouldBe` "one hundred and two"
+  it "f2" $ f2 0200 `shouldBe` "two hundred"
+  it "f2" $ f2 0111 `shouldBe` "one hundred and eleven"
+  it "f2" $ f2 0115 `shouldBe` "one hundred and fifteen"
+  it "f2" $ f2 0901 `shouldBe` "nine hundred and one"
+  it "f2" $ f2 0342 `shouldBe` "three hundred and forty-two"
+  it "f2" $ f2 1000 `shouldBe` "one thousand"
+  it "f2" $ f2 1001 `shouldBe` "one thousand and one"
+  it "f2" $ f2 1002 `shouldBe` "one thousand and two"
+  it "f2" $ f2 1011 `shouldBe` "one thousand and eleven"
+  it "f2" $ f2 1021 `shouldBe` "one thousand and twenty-one"
+  it "f2" $ f2 1121 `shouldBe` "one thousand and one hundred and twenty-one"
+  it "f2" $ f2 1111 `shouldBe` "one thousand and one hundred and eleven"
+  it "f2" $ f2 2000 `shouldBe` "two thousand"
+  it "f2" $ f2 2001 `shouldBe` "two thousand and one"
+  it "f2" $ f2 2002 `shouldBe` "two thousand and two"
+  it "f2" $ f2 2111 `shouldBe` "two thousand and one hundred and eleven"
   it "f3" $ f3 1 `shouldBe` (0, 0, 0, 1)
   it "f3" $ f3 5 `shouldBe` (0, 0, 0, 5)
   it "f3" $ f3 10 `shouldBe` (0, 0, 1, 0)
