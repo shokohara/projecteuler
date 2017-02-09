ae8fe5013b8653845e9189676e55ba9c9501a45a
shokohara <shokohara@users.noreply.github.com>
2017-02-09 12:02:12 +0900

    wip

--- src/Projecteuler/Problem18.hs
+++ src/Projecteuler/Problem18.hs
@@ -17,28 +17,11 @@ x = fmap (fmap digitToInt) . concatMap words . lines $ [here|3
 xx :: [[(Int, Int)]]
 xx = zip [0..] <$> x
 
-z :: [[Int]] -> [Int] -> [[Int]]
-z ass bs = concatMap (\as -> fmap (\b -> as ++ [b]) . filter (last as <=) . filter (last as + 1 >=) $ bs) ass
-
 a :: [[Int]]
 a = (\v -> 0 : [v]) <$> [0..1]
 
---b :: [[Int]] -> [Int] -> [[Int]]
---b ass bs = concatMap (\x -> fmap (\v -> x ++ [v]) $ take 2 $ drop (last x) $ bs) ass
---b :: [[Int]]
---b = concatMap (\x -> fmap (\v -> x ++ [v]) . take 2 . drop (last x) [0..2]) [[0,0],[0,1]]
---b = concatMap (\x -> trace' $ fmap (\v -> x ++ [v]) $ filter (last x + 1 >=) . filter (last x <=) $ [0..2]) b
 d :: [[Int]] -> [Int] -> [[Int]]
-d ass bs = concatMap (\x -> fmap (\v -> x ++ [v]) . take 2 . drop (last x) $ bs) ass
-
---c = concatMap (\x -> trace' $ fmap (\v -> x ++ [v]) $ take 2 $ drop (last x) $ [0..3]) b
---c = fmap (\x -> trace' $ fmap (\v -> x ++ [v]) $ filter (last x + 1 >=) . filter (last x <=) $ [0..3]) b
-y :: Int
---y = sum . foldl (\xs x -> 0) 0 xx
-y = sum . fmap (\xs -> sum . fmap snd . take 2 . drop (length xs - 1) $ xs) $ xx
---y = sum . fmap (\xs -> (\(v) -> foldl (+) 0 v) . take 2 . drop (length xs - 1) $ xs) $ x
---y = sum . fmap (\xs -> (\(i, v) -> foldl (+) 0 v) . take 2 . drop (length xs - 1) $ xs) $ xx
---y = sum $ fmap maximum x
+d ass bs = concatMap (\x -> fmap (\v -> x ++ [v]) . take 2 . drop (sum . take 1 . reverse $ x) $ bs) ass
 
 resolve :: Integer
 resolve = toInteger $ sum $ fmap maximum x
